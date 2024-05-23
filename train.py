91439ba1383394cd93d3ab1b33ea4e92039aaacf
Nora Belrose <belrose.nora@gmail.com>
2024-05-23 17:59:26 +0000

    Higher LR, etc.

--- train.py
+++ train.py
@@ -20,7 +20,6 @@ from transformers import (
 import wandb
 from w2s.ds_registry import load_and_process_dataset
 from w2s.knn import gather_hiddens, topofilter
-from w2s.loss import log_confidence_loss
 from w2s.roc_auc import roc_auc
 
 disable_caching()
@@ -55,23 +54,10 @@ class DistillationTrainer(Trainer):
         labels = inputs.pop("labels").float()
 
         outputs = model(**inputs)
-        loss = log_confidence_loss(outputs.logits, labels, self.state.global_step)
 
-        # labels = torch.stack([1.0 - labels, labels], dim=-1)
-        # loss = torch.nn.functional.cross_entropy(outputs.logits, labels)
+        labels = torch.stack([1.0 - labels, labels], dim=-1)
+        loss = torch.nn.functional.cross_entropy(outputs.logits, labels)
         return (loss, outputs) if return_outputs else loss
-
-
-# Works for Llama, Mistral, and Qwen architectures
-LORA_MODULES = [
-    "gate_proj",
-    "down_proj",
-    "up_proj",
-    "q_proj",
-    "k_proj",
-    "v_proj",
-    "o_proj",
-]
 
 
 def move_best_ckpt(trainer: Trainer):
@@ -87,7 +73,14 @@ def move_best_ckpt(trainer: Trainer):
 
 
 def train(cfg: TrainConfig):
-    lora_cfg = LoraConfig(target_modules=LORA_MODULES)
+    # Works for Llama, Mistral, and Qwen architectures
+    PEFT_MODULES = [
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+    ]
+    peft_cfg = LoraConfig(modules_to_save=["score"], target_modules=PEFT_MODULES)
 
     # for 2 strong models we do WEAK -> STRONG[0] -> STRONG[1] -> STRONG[0] -> ...
     STRONG_NAMES = ["meta-llama/Meta-Llama-3-8B", "mistralai/Mistral-7B-v0.1"]
@@ -112,6 +105,12 @@ def train(cfg: TrainConfig):
         )
         model.config.pad_token_id = strong_tokenizers[name].pad_token_id  # type: ignore
         model.score.weight.data *= 0.01
+        return model
+
+    def make_peft(model):
+        model = get_peft_model(model, peft_cfg)
+        model.print_trainable_parameters()
+
         return model
 
     def weak_processor(examples):
@@ -140,24 +139,23 @@ def train(cfg: TrainConfig):
 
     root = Path("results") / cfg.dataset
     train_cfg = dict(
-        output_dir=str(root / "floor"),
-        num_train_epochs=2,
         adam_beta2=0.95,
-        gradient_accumulation_steps=8 // cfg.minibatch_size,
         evaluation_strategy="epoch",
+        gradient_accumulation_steps=8 // cfg.minibatch_size,
         label_names=["labels"],
+        learning_rate=5e-4,
         load_best_model_at_end=True,
         logging_steps=50,
         metric_for_best_model="auroc",
-        per_device_train_batch_size=cfg.minibatch_size,
+        num_train_epochs=3,
+        output_dir=str(root / "floor"),
         per_device_eval_batch_size=cfg.minibatch_size,
+        per_device_train_batch_size=cfg.minibatch_size,
         run_name=cfg.dataset + "/floor" + cfg.run_name,
         save_strategy="epoch",
         save_total_limit=1,
         tf32=True,  # Use Tensor Cores even for fp32 matmuls
-        warmup_steps=100,
-        weight_decay=0.01,
-        learning_rate=3e-5,
+        warmup_steps=40,
     )
 
     # Gather weak labels
@@ -169,7 +167,7 @@ def train(cfg: TrainConfig):
     else:
         should_train = True
         weak_path = root / "floor/best-ckpt"
-        if not weak_path.exists():
+        if weak_path.exists():
             weak_model = AutoModelForSequenceClassification.from_pretrained(
                 cfg.weak_name, torch_dtype="auto"
             )
@@ -177,7 +175,7 @@ def train(cfg: TrainConfig):
             # HuggingFace init for the head is too large
             weak_model.score.weight.data *= 0.01
             weak_model.config.problem_type = "single_label_classification"
-            weak_model = get_peft_model(weak_model, lora_cfg)
+            weak_model = make_peft(weak_model)
         else:
             print("Loading weak model from:", weak_path)
             should_train = False
@@ -238,6 +236,9 @@ def train(cfg: TrainConfig):
         for name in STRONG_NAMES
     }
 
+    # Lower learning rate for the larger model
+    train_cfg["learning_rate"] = 2e-4
+
     strong_ckpt = root / "ceil" / "best-ckpt"
     if strong_ckpt.exists():
         print(f"Strong ceiling model already exists at {strong_ckpt}")
@@ -251,7 +252,7 @@ def train(cfg: TrainConfig):
             compute_metrics=compute_metrics,
             data_collator=DataCollatorWithPadding(strong_tokenizers[MAIN_STRONG_NAME]),
             eval_dataset=ceil_tests[MAIN_STRONG_NAME],
-            model=get_peft_model(init_strong_model(MAIN_STRONG_NAME), lora_cfg),
+            model=make_peft(init_strong_model(MAIN_STRONG_NAME)),
             tokenizer=strong_tokenizers[MAIN_STRONG_NAME],
             train_dataset=strong_trains[MAIN_STRONG_NAME],
         )
@@ -263,15 +264,22 @@ def train(cfg: TrainConfig):
     w2s_train = strong_trains[MAIN_STRONG_NAME].remove_columns("labels")
     w2s_train = w2s_train.add_column("labels", train_probs.numpy())
 
-    # Check gt metrics every 100 steps during w2s training.
-    # We can overfit to the weak labels before a single epoch.
-    train_cfg["evaluation_strategy"] = "steps"
-    train_cfg["save_strategy"] = "steps"
-    train_cfg["eval_steps"] = 100
-    train_cfg["save_steps"] = 100
-    train_cfg["label_names"] = ["labels"]
-    train_cfg["output_dir"] = str(root / ("w2s" + cfg.run_name))
-    train_cfg["run_name"] = cfg.dataset + "/w2s" + cfg.run_name
+    train_cfg.update(
+        # Check gt metrics every 100 steps during w2s training.
+        # We can overfit to the weak labels before a single epoch.
+        eval_steps=250,
+        evaluation_strategy="steps",
+        # Use a 4x larger batch size, performing fewer training steps
+        # given the same number of examples to reduce overfitting
+        # to the weak model's errors.
+        gradient_accumulation_steps=32 // cfg.minibatch_size,
+        label_names=["labels"],
+        num_train_epochs=3,
+        output_dir=str(root / ("w2s" + cfg.run_name)),
+        run_name=cfg.dataset + "/w2s" + cfg.run_name,
+        save_steps=250,
+        save_strategy="steps",
+    )
 
     should_train = True
     w2s_ckpt = root / ("w2s" + cfg.run_name) / "best-ckpt"
@@ -300,7 +308,7 @@ def train(cfg: TrainConfig):
             indices = topofilter(train_acts, y, cfg.contamination, k=cfg.outlier_k)
             w2s_train = w2s_train.select(indices)
 
-        w2s_model = get_peft_model(strong_model, lora_cfg)
+        w2s_model = make_peft(strong_model)
 
     w2s_model.config.pad_token_id = strong_tokenizers[MAIN_STRONG_NAME].pad_token_id  # type: ignore # noqa
     trainer = DistillationTrainer(
@@ -354,7 +362,7 @@ def train(cfg: TrainConfig):
             compute_metrics=compute_metrics,
             data_collator=DataCollatorWithPadding(strong_tokenizers[strong_name]),
             eval_dataset=ceil_tests[strong_name],
-            model=get_peft_model(init_strong_model(strong_name), lora_cfg),
+            model=make_peft(init_strong_model(strong_name)),
             tokenizer=strong_tokenizers[strong_name],
             train_dataset=w2s_train,
         )
